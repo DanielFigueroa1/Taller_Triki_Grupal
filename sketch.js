f13c9151924d35bfb42c380625e8bce21bba5f8e
[DanielFigueroa1] <[daniel.alejandroolmedo@outlook.es]>
2020-02-13 14:46:07 -0500

    Main

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let tablero = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+
+]
+
+let figuras = [];
+
+function setup() {
+
+  createCanvas(600, 600);
+
+}
+
+function draw() {
+
+}
+
+function mousePressed() {
+
+}
