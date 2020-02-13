b799afe51c7946890dbbf6085f805fba7d9baa9e
[DanielFigueroa1] <[daniel.alejandroolmedo@outlook.es]>
2020-02-13 15:00:05 -0500

    Draw

--- sketch.js
+++ sketch.js
@@ -15,6 +15,54 @@ function setup() {
 
 function draw() {
 
+
+  console.log(pantalla);
+
+  switch (pantalla) {
+
+
+    case 1:
+
+      background(255, 0, 255);
+
+      textSize(50);
+      text("Gana jugador 1", 150, 300);
+
+      Reset();
+
+
+      break;
+
+    case 2:
+
+      textSize(50);
+
+      background(255, 0, 255);
+      text("Gana jugador 2", 150, 300)
+
+      Reset();
+
+
+      break;
+
+    case 3:
+  
+      for (let i = 0; i < figuras.length; i++) {
+
+
+        //pinta todas las figuras
+
+        figuras[i].Pintar();
+
+      }
+  
+      Ganar();
+
+      break;
+
+
+  }
+
 }
 
 function mousePressed() {
