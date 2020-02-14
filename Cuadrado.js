ec64007a58e34df297ce8428def7a38ce7f6ec70
DianaGiraldo13 <dianasofiagiraldobotello@gmail.com>
2020-02-13 20:59:01 -0500

    Se creo la clase cuadrado y se implemento

---
+++ Cuadrado.js
@@ -0,0 +1,24 @@
+class Cuadro extends Figura{
+  
+  
+    constructor(x,y){
+    
+      super(x,y);
+    
+    
+    
+    
+    }
+ 
+ 
+ 
+ 
+ Pintar(){
+    
+     rectMode(CENTER);
+     rect(this.x,this.y,100,100);
+    
+    }
+ 
+ 
+ }
